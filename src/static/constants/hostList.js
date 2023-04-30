990b0582904c6e67d29ee43ce088de47a37a311c
Эмиль Астанов <emilastanov@MBP-Emil.Dlink>
2023-04-30 12:23:13 +0300

    Updated host

--- src/static/constants/hostList.js
+++ src/static/constants/hostList.js
@@ -1,5 +1,5 @@
 
-export const SAPER_HOST = 'https://188.120.251.68';
+export const SAPER_HOST = 'https://emila1998.fvds.ru';
 
 export const SAPER_METHODS = {
     METRICS: SAPER_HOST + "/user-activity-metrics"
