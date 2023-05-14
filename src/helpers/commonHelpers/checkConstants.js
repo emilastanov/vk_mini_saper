d8fbcb0e1c51974b5098941a4d7da4e0bc6431b6
Эмиль Астанов <emilastanov@MBP-Emil.Dlink>
2023-05-14 16:35:30 +0300

    Bug fixed

--- src/helpers/commonHelpers/checkConstants.js
+++ src/helpers/commonHelpers/checkConstants.js
@@ -1,6 +1,6 @@
 
 export function checkConstants(board, size) {
     const bombCount = board.flat().reduce((acc, item)=>(acc+item), 0);
-    const width = board[0].length();
+    const width = board[0].length;
     return `${size}${bombCount}-${width}`;
 }
